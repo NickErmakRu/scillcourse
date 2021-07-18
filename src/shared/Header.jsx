0f416f33481982d0f576ccf853d148101d050246
Nick Ermak <nk995@mail.ru>
2021-07-18 22:43:51 +0300

    css-modules added

--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -1,10 +1,11 @@
 import * as React from "react";
 import { hot } from "react-hot-loader/root";
+import styles from './header.css';
 
 function HeaderComponent() {
   return (
     <header>
-      <h1>Reddit for our own</h1>
+      <h1 className={ styles.example }>Reddit for our own</h1>
       <p>Hello there</p>
     </header>
   )
