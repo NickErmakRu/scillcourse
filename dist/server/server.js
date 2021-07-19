5ed0ca4e18eab4c4e7d5d97f580bc9344fa62eb9
Nick Ermak <nk995@mail.ru>
2021-07-20 01:03:45 +0300

    starWarsExample added

--- dist/server/server.js
+++ dist/server/server.js
@@ -118,7 +118,30 @@ eval("\r\nvar __importDefault = (this && this.__importDefault) || function (mod)
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-eval("\r\nvar __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {\r\n    if (k2 === undefined) k2 = k;\r\n    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });\r\n}) : (function(o, m, k, k2) {\r\n    if (k2 === undefined) k2 = k;\r\n    o[k2] = m[k];\r\n}));\r\nvar __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {\r\n    Object.defineProperty(o, \"default\", { enumerable: true, value: v });\r\n}) : function(o, v) {\r\n    o[\"default\"] = v;\r\n});\r\nvar __importStar = (this && this.__importStar) || function (mod) {\r\n    if (mod && mod.__esModule) return mod;\r\n    var result = {};\r\n    if (mod != null) for (var k in mod) if (k !== \"default\" && Object.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);\r\n    __setModuleDefault(result, mod);\r\n    return result;\r\n};\r\nvar __importDefault = (this && this.__importDefault) || function (mod) {\r\n    return (mod && mod.__esModule) ? mod : { \"default\": mod };\r\n};\r\nObject.defineProperty(exports, \"__esModule\", { value: true });\r\nexports.Header = void 0;\r\nvar React = __importStar(__webpack_require__(/*! react */ \"react\"));\r\nvar root_1 = __webpack_require__(/*! react-hot-loader/root */ \"react-hot-loader/root\");\r\nvar header_css_1 = __importDefault(__webpack_require__(/*! ./header.css */ \"./src/shared/header.css\"));\r\nfunction HeaderComponent() {\r\n    return (React.createElement(\"header\", null,\r\n        React.createElement(\"h1\", { className: header_css_1.default.example }, \"Reddit for our own\"),\r\n        React.createElement(\"p\", null, \"Hello there\")));\r\n}\r\nexports.Header = root_1.hot(HeaderComponent);\r\n\n\n//# sourceURL=webpack:///./src/shared/Header.jsx?");
+eval("\r\nvar __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {\r\n    if (k2 === undefined) k2 = k;\r\n    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });\r\n}) : (function(o, m, k, k2) {\r\n    if (k2 === undefined) k2 = k;\r\n    o[k2] = m[k];\r\n}));\r\nvar __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {\r\n    Object.defineProperty(o, \"default\", { enumerable: true, value: v });\r\n}) : function(o, v) {\r\n    o[\"default\"] = v;\r\n});\r\nvar __importStar = (this && this.__importStar) || function (mod) {\r\n    if (mod && mod.__esModule) return mod;\r\n    var result = {};\r\n    if (mod != null) for (var k in mod) if (k !== \"default\" && Object.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);\r\n    __setModuleDefault(result, mod);\r\n    return result;\r\n};\r\nvar __importDefault = (this && this.__importDefault) || function (mod) {\r\n    return (mod && mod.__esModule) ? mod : { \"default\": mod };\r\n};\r\nObject.defineProperty(exports, \"__esModule\", { value: true });\r\nexports.Header = void 0;\r\nvar React = __importStar(__webpack_require__(/*! react */ \"react\"));\r\nvar root_1 = __webpack_require__(/*! react-hot-loader/root */ \"react-hot-loader/root\");\r\nvar header_css_1 = __importDefault(__webpack_require__(/*! ./header.css */ \"./src/shared/header.css\"));\r\nvar StarWarsNameClass_1 = __webpack_require__(/*! ./StateExamples/StarWarsNameClass/StarWarsNameClass */ \"./src/shared/StateExamples/StarWarsNameClass/StarWarsNameClass.tsx\");\r\nfunction HeaderComponent() {\r\n    return (React.createElement(\"header\", null,\r\n        React.createElement(\"h1\", { className: header_css_1.default.example }, \"Reddit for our own\"),\r\n        React.createElement(\"p\", null, \"Hello there\"),\r\n        React.createElement(StarWarsNameClass_1.StarWarsNameClass, null)));\r\n}\r\nexports.Header = root_1.hot(HeaderComponent);\r\n\n\n//# sourceURL=webpack:///./src/shared/Header.jsx?");
+
+/***/ }),
+
+/***/ "./src/shared/StateExamples/StarWarsNameClass/StarWarsNameClass.tsx":
+/*!**************************************************************************!*\
+  !*** ./src/shared/StateExamples/StarWarsNameClass/StarWarsNameClass.tsx ***!
+  \**************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+eval("\r\nvar __extends = (this && this.__extends) || (function () {\r\n    var extendStatics = function (d, b) {\r\n        extendStatics = Object.setPrototypeOf ||\r\n            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||\r\n            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };\r\n        return extendStatics(d, b);\r\n    };\r\n    return function (d, b) {\r\n        extendStatics(d, b);\r\n        function __() { this.constructor = d; }\r\n        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());\r\n    };\r\n})();\r\nvar __importDefault = (this && this.__importDefault) || function (mod) {\r\n    return (mod && mod.__esModule) ? mod : { \"default\": mod };\r\n};\r\nObject.defineProperty(exports, \"__esModule\", { value: true });\r\nexports.StarWarsNameClass = void 0;\r\nvar react_1 = __importDefault(__webpack_require__(/*! react */ \"react\"));\r\nvar unique_names_generator_1 = __webpack_require__(/*! unique-names-generator */ \"unique-names-generator\");\r\nvar styles = __webpack_require__(/*! ./starWarsNameClass.css */ \"./src/shared/StateExamples/StarWarsNameClass/starWarsNameClass.css\");\r\nvar StarWarsNameClass = /** @class */ (function (_super) {\r\n    __extends(StarWarsNameClass, _super);\r\n    function StarWarsNameClass() {\r\n        var _this = _super !== null && _super.apply(this, arguments) || this;\r\n        _this.state = {\r\n            name: _this.randomName(),\r\n            count: 0\r\n        };\r\n        _this.handleClick = function () {\r\n            _this.setState({ name: _this.randomName() });\r\n            _this.setState(function (state, props) { return ({ count: state.count + 1 }); });\r\n        };\r\n        return _this;\r\n    }\r\n    StarWarsNameClass.prototype.render = function () {\r\n        console.log('>>', this.state.count);\r\n        return (react_1.default.createElement(\"section\", { className: styles.container },\r\n            react_1.default.createElement(\"span\", { className: styles.name }, this.state.name),\r\n            react_1.default.createElement(\"div\", { className: styles.gap }),\r\n            react_1.default.createElement(\"button\", { className: styles.button, onClick: this.handleClick }, \"\\u041C\\u043D\\u0435 \\u043D\\u0443\\u0436\\u043D\\u043E \\u0438\\u043C\\u044F!\")));\r\n    };\r\n    StarWarsNameClass.prototype.randomName = function () {\r\n        return unique_names_generator_1.uniqueNamesGenerator({ dictionaries: [unique_names_generator_1.starWars], length: 1 });\r\n    };\r\n    return StarWarsNameClass;\r\n}(react_1.default.PureComponent));\r\nexports.StarWarsNameClass = StarWarsNameClass;\r\n\n\n//# sourceURL=webpack:///./src/shared/StateExamples/StarWarsNameClass/StarWarsNameClass.tsx?");
+
+/***/ }),
+
+/***/ "./src/shared/StateExamples/StarWarsNameClass/starWarsNameClass.css":
+/*!**************************************************************************!*\
+  !*** ./src/shared/StateExamples/StarWarsNameClass/starWarsNameClass.css ***!
+  \**************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("// Exports\nmodule.exports = {\n\t\"container\": \"starWarsNameClass__container--GcX_p\",\n\t\"gap\": \"starWarsNameClass__gap--_Y12n\",\n\t\"button\": \"starWarsNameClass__button--XN247\",\n\t\"name\": \"starWarsNameClass__name--1xRPs\"\n};\n\n\n//# sourceURL=webpack:///./src/shared/StateExamples/StarWarsNameClass/starWarsNameClass.css?");
 
 /***/ }),
 
@@ -174,6 +197,17 @@ eval("module.exports = require(\"react-dom/server\");\n\n//# sourceURL=webpack:/
 /***/ (function(module, exports) {
 
 eval("module.exports = require(\"react-hot-loader/root\");\n\n//# sourceURL=webpack:///external_%22react-hot-loader/root%22?");
+
+/***/ }),
+
+/***/ "unique-names-generator":
+/*!*****************************************!*\
+  !*** external "unique-names-generator" ***!
+  \*****************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("module.exports = require(\"unique-names-generator\");\n\n//# sourceURL=webpack:///external_%22unique-names-generator%22?");
 
 /***/ })
 
--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -2,11 +2,14 @@ import * as React from "react";
 import { hot } from "react-hot-loader/root";
 import styles from './header.css';
 
+import { StarWarsNameClass } from "./StateExamples/StarWarsNameClass/StarWarsNameClass";
+
 function HeaderComponent() {
   return (
     <header>
       <h1 className={ styles.example }>Reddit for our own</h1>
       <p>Hello there</p>
+      <StarWarsNameClass />
     </header>
   )
 }
